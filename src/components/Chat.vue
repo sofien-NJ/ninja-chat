a6cc843ba0891ed49e0bf8a600afe7bf64958986
sofien njeimi <7sofien@gmail.com>
2021-09-30 22:04:21 +0100

    update

---
+++ src/components/Chat.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="chat container">
+    <h2>Chat</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: "Chat",
+  data() {
+    return {};
+  }
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
-
+import Chat from '@/components/Chat'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'Welcome',
       component: Welcome
+    },
+    {
+      path: '/chat',
+      name: 'Chat',
+      component: Chat
     }
   ]
 })
